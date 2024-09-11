4ba48ad3158fdd3d3db0b30a5fda884fca6f76ac
DimensionPlant1 <svencallumreber@gmail.com>
2024-09-11 20:04:43 +0200

    Adding the missing Js files

---
+++ JS/PortfolioFunc.js
@@ -0,0 +1,40 @@
+
+var language = "english";
+var Content = Content.JSON;
+var ongoing = 
+    [
+    {"title": "Portfolio","description":"blurb"},
+    {"title": "Of the Deep End","description":"blurb"}
+    ];
+
+var Finished = 
+    [
+    {"title":"Football Manager", "description":"blurb"},
+    {"title":"Animieren einer Komplexen Figur", "description":"blurb"}
+    ];
+
+function adjust()
+{
+
+}
+//method to load text
+function loadButton()
+{
+
+}
+
+function exchange()
+{
+    //fade out
+    //animate
+    //fade in
+}
+
+function animate()
+{
+    
+}
+
+{
+
+}
