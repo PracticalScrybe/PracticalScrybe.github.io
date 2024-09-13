65ce6859d42b4425f07bf576b18ebba144d3fd39
DimensionPlant1 <svencallumreber@gmail.com>
2024-09-13 12:04:53 +0200

    First attempt at buttons
    Let's see if the buttons actually append or if the script still refuses to load. I'd prefer it not to be obstinate

--- JS/PortfolioFunc.js
+++ JS/PortfolioFunc.js
@@ -1,17 +1,22 @@
+var drop = document.getElementById("language");
+var language = drop.options[drop.selectedIndex].text;
+var flip = false;
+const response = await fetch("./JS/Content",
+    {
+    headers:
+    {
+      'Accept': 'application/json'
+    }
+  });
+const content = await response.json();
+console.log(content);
 
-var language = "english";
-var Content = Content.JSON;
-var ongoing = 
-    [
-    {"title": "Portfolio","description":"blurb"},
-    {"title": "Of the Deep End","description":"blurb"}
-    ];
 
-var Finished = 
-    [
-    {"title":"Football Manager", "description":"blurb"},
-    {"title":"Animieren einer Komplexen Figur", "description":"blurb"}
-    ];
+function lighten(str)
+{
+    //let's see if this works
+    document.getElementById(str).style.backgroundColor = "#000000";
+}
 
 function adjust()
 {
@@ -19,7 +24,27 @@ function adjust()
 }
 
 //method to load text
-function loadButton()
+function loadButtons(stats)
+{
+    //ongoing and finished
+    for(const stat of stats)
+    {
+        for (let index = 0; index < content["projects"][language][stat].length; ++index) 
+            {
+            const element = content["projects"][language][stat][index];
+    
+            var button = document.createElement("button");
+            button.addEventListener("click", exchange());
+            button.addEventListener("onmouseover ", lighten(element["title"]));
+    
+            var buttons = document.getElementById(stat);
+            buttons.appendChild(button);
+        }
+    }
+    
+}
+
+function updateButtons()
 {
 
 }
@@ -29,6 +54,8 @@ function exchange()
     //fade out
     //animate
     //fade in
+    console.log("checks out");
+    flip = true;
 }
 
 function animate()
@@ -37,7 +64,7 @@ function animate()
 }
 
 {
-    fetch("./JS/Content.json")
-        .then((response) => response.json())
-        .then((json) => console.log(json));
+    console.log(language);
+    const stats = ["ongoing","finished"];
+    loadButtons(stats);
 }
